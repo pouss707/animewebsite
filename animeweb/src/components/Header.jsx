ed7fce026b036909f78ae89879385178fe752528
pouss707 <166956207+pouss707@users.noreply.github.com>
2025-05-08 22:08:58 +0100

    updated the header started with center img

--- animeweb/src/components/Header.jsx
+++ animeweb/src/components/Header.jsx
@@ -1,24 +1,40 @@
 import React from 'react'
 import logo from '../assets/logo.png'
 import { Link } from 'react-router-dom'
+import SearchIcon from '@mui/icons-material/Search';
+import Person4Icon from '@mui/icons-material/Person4';
 function Header() {
     return (
         <div>
             <header className='bg-blue-400 text-black flex justify-between items-center p-[10px] h-[70px] w-screen shadow-[0_0_0_0.05_black] font-bold'>
-                <img className='h-[60px] w-[60px]' src={logo} alt="logo" />
-                <nav className='mr-[10px] p-[10px]'>
-                    <ul className="flex gap-[10px] list-image-none">
-                        <li>blog</li>
-                        <li>contact</li>
-                        <li>
-                            <ul>
-                                <Link></Link>
-                            </ul>
-                        </li>
-                    </ul>
-                </nav>
+                <Link to="/"><img className='h-[60px] w-[60px] ml-[10px]' src={logo} alt="logo" /></Link>
+                <div className='flex justify-center items-center ml-[8px]'>
+                    <nav className='p-[10px]'>
+                        <ul className="flex gap-[30px] list-image-none">
+                            <select name="categories">
+                                <option value="">categories</option>
+                                <option value="action">action</option>
+                                <option value="adventure">adventure</option>
+                                <option value="comedy">comedy</option>
+                                <option value="drama">drama</option>
+                                <option value="fantasy">fantasy</option>
+                                <option value="horror">horror</option>
+                                <option value="mystery">mystery</option>
+                                <option value="romance">romance</option>
+                                <option value="sci-fi">sci-fi</option>
+
+                            </select>
+                            <li>blog</li>
+                            <li>contact</li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className='flex justify-center items-center gap-[20px] mr-[10px]'>
+                    <button className='cursor-pointer'><SearchIcon /></button>
+                    <button className='cursor-pointer'><Person4Icon /></button>
+                </div>
             </header>
-        </div>
+        </div >
     )
 }
 
--- animeweb/src/components/Home.jsx
+++ animeweb/src/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './Header'
-
+import testimg from '../assets/test.png'
 function Home() {
     return (
         <div>
             <Header />
+            <div className='flex justify-center items-center h-[500px] w-screen pt-[208px]' >
+                <img className='h-[608px] w-[1200px]' src={testimg} alt="test" />
+            </div>
         </div>
     )
 }
